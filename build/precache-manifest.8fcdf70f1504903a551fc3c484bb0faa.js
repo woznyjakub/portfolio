f4220551352ca9506110c65da641009dab5ab073
Jakub Woźny <kuba@MacBook-Pro-Jakub.local>
2019-11-24 01:56:46 +0100

    deploy placeholder page

---
+++ build/precache-manifest.8fcdf70f1504903a551fc3c484bb0faa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fede4a1fe6acd3a635de88578b5287c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "37fda83450de6fc5b114",
+    "url": "/portfolio/static/js/2.c016d384.chunk.js"
+  },
+  {
+    "revision": "5cdff18e700a95473684",
+    "url": "/portfolio/static/js/main.eed3673d.chunk.js"
+  },
+  {
+    "revision": "3f4cee59d45b893f9d2c",
+    "url": "/portfolio/static/js/runtime-main.5f490551.js"
+  }
+]);
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PlaceholderPage from './PlaceholderPage.jsx';
-
-ReactDOM.render(<PlaceholderPage />, document.getElementById('root'));
