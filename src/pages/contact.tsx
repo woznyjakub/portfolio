38163a21a7200bd29063156c9b6b055d97baafa4
Jakub Woźny <jakub_wozny_01@wp.pl>
2020-07-28 23:59:12 +0200

    pages components migration

---
+++ src/pages/contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { BasicLayout } from '../components';
+
+const ContactPage = () => {
+  return (
+    <BasicLayout>
+      <div>
+        <p>TEST: page contact</p>
+      </div>
+    </BasicLayout>
+  );
+};
+
+export default ContactPage;
---
+++ src/pages/skills.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import styled from 'styled-components';
+import { media } from '../utils';
+import { BasicLayout, Heading, BasicText, Footer } from '../components';
+
+const content = {
+  skillsGroups: [
+    {
+      name: 'JavaScript',
+      skills: ['JavaScript ES6/7/8+', 'TypeScript', 'React', 'GatsbyJS', 'Redux'],
+    },
+    {
+      name: 'CSS',
+      skills: ['CSS', 'Sass', 'Styled Components', 'Less', 'Bootstrap 4'],
+    },
+    {
+      name: 'HTML',
+      skills: ['Semantic HTML', 'Pug', 'Handlebars', 'Accessibility (WCAG 2.1)'],
+    },
+    {
+      name: 'Other',
+      skills: ['Git (and Git Flow)', 'Webpack', 'Gulp', 'Adobe Xd', 'Figma', 'Photoshop', 'JIRA'],
+    },
+  ],
+};
+
+const SkillsList = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1rem;
+  max-width: 800px;
+  margin: 0 auto;
+  ${media.phone`
+    grid-template-columns: repeat(2, 1fr);
+  `};
+`;
+
+const SkillsListItem = styled.li`
+  border: 1px solid currentColor;
+  padding: 1rem;
+
+  ${media.desktopSmall`
+    padding: 2rem;
+  `}
+`;
+
+const NestedListItemText = styled(BasicText)`
+  line-height: 1.2;
+  && {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const SkillsPage = () => {
+  return (
+    <BasicLayout>
+      <div className="layout-wrapper">
+        <header>
+          <Heading as="h1" fontSize="large" gutter="bottom" centered>
+            Skills
+          </Heading>
+        </header>
+        <main>
+          <section>
+            <SkillsList className="list-unstyled">
+              {content.skillsGroups.map(({ name, skills }, i) => (
+                <SkillsListItem key={`${name}-${i}`}>
+                  <Heading gutter="bottom">{name}</Heading>
+                  <ul>
+                    {skills.map((skill, i) => (
+                      <li key={`${skill}-${i}`}>
+                        <NestedListItemText>{skill}</NestedListItemText>
+                      </li>
+                    ))}
+                  </ul>
+                </SkillsListItem>
+              ))}
+            </SkillsList>
+          </section>
+        </main>
+        <Footer />
+      </div>
+    </BasicLayout>
+  );
+};
+
+export default SkillsPage;
