8b41d70eb44c25f7a257ef094e153cee48d7d52c
Jakub Woźny <jakub_wozny_01@wp.pl>
2020-10-24 21:22:12 +0200

    OptionalWrapper type and props names update

--- src/containers/misc/optional-wrapper/OptionalWrapper.tsx
+++ src/containers/misc/optional-wrapper/OptionalWrapper.tsx
@@ -2,12 +2,13 @@ import React, { ComponentType } from 'react';
 
 interface OptionalWrapperCmpProps {
   condition: boolean;
-  WrapperComponent: ComponentType;
-  children: any;
+  Component: ComponentType;
+  // @todo: check why removing children prop type below causes ts errors
+  children?: any;
 }
 
-const OptionalWrapper: React.FC<OptionalWrapperCmpProps> = ({ condition, WrapperComponent, children }) => {
-  return condition ? <WrapperComponent>{children}</WrapperComponent> : children;
+const OptionalWrapper: React.FC<OptionalWrapperCmpProps> = ({ condition, Component, children }) => {
+  return condition ? <Component>{children}</Component> : children;
 };
 
 export default OptionalWrapper;
