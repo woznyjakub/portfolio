ac26999e4b4567b271391f275b1521bbcdf3f983
Jakub Woźny <jakub_wozny_01@wp.pl>
2020-11-26 20:43:58 +0100

    skills page - implement sourcing textual data from GraphQL

---
+++ src/_pages/skills/skills.model.ts
@@ -0,0 +1,15 @@
+import { JsonTextData } from '../../models/graphql';
+
+export interface SkillItem {
+  name: string;
+  skills: string[];
+}
+
+export interface TextContent {
+  skillsGroups: SkillItem[];
+  skillsGroupsAltText: string;
+}
+
+export interface SkillsPageProps {
+  data?: JsonTextData<TextContent>;
+}
--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,1 +1,20 @@
+import { graphql } from 'gatsby';
+
 export { default } from '../_pages/skills';
+
+export const query = graphql`
+  {
+    allDataJson(filter: { pageName: { eq: "skills" } }) {
+      nodes {
+        pageName
+        content {
+          skillsGroups {
+            name
+            skills
+          }
+          skillsGroupsAltText
+        }
+      }
+    }
+  }
+`;
