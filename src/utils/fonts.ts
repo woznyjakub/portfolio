01dfe45e4796600ef58391ddebafedd66d94c317
Jakub Woźny <jakub_wozny_01@wp.pl>
2020-11-26 23:52:10 +0100

    add enum for fonts

---
+++ src/utils/fonts.ts
@@ -0,0 +1,4 @@
+export enum Font {
+  PRIMARY = `'DM Serif Text', -apple-system, BlinkMacSystemFont, Roboto, 'Liberation Sans', Ubuntu, 'Open Sans', 'Helvetica Neue', Arial, sans-serif`,
+  SECONDARY = `'caslongrad', serif`,
+}
